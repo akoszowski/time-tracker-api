c5e5c931b6e8049f7a4a72f87dfe09af1dc68316
akoszowski <a.koszowski@students.mimuw.edu.pl>
2021-07-25 15:28:20 +0200

    Add tracker.resolver unit tests.

--- src/tracker/tracker.resolver.spec.ts
+++ src/tracker/tracker.resolver.spec.ts
@@ -1,18 +1,165 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrackerResolver } from './tracker.resolver';
+import { TrackerService } from './tracker.service';
+import { Status, Task } from './models/task.model';
+import { Timer } from './models/timer.model';
 
 describe('TrackerResolver', () => {
   let resolver: TrackerResolver;
 
+  const timerInfo = {
+    id: 1,
+    startTime: new Date(),
+    finishTime: undefined,
+  };
+
+  const timer: Timer = {
+    ...timerInfo,
+    taskId: 1,
+  };
+
+  const task: Task = {
+    id: 1,
+    name: 'Startup House recruitment task',
+    description: 'Time tracker back-end API',
+    status: Status.ACTIVE,
+    timer: timer,
+  };
+
+  const finishedTasks: Task[] = [
+    {
+      id: 5,
+      name: 'First finished task',
+      description: 'Some description',
+      status: Status.STOPPED,
+      timer: {
+        id: Math.random(),
+        startTime: new Date(2021, 6, 23),
+        finishTime: new Date(2021, 6, 24),
+        taskId: 5,
+      },
+    },
+    {
+      id: 6,
+      name: 'Second finished task',
+      description: 'Some description',
+      status: Status.STOPPED,
+      timer: {
+        id: Math.random(),
+        startTime: new Date(2021, 6, 24),
+        finishTime: new Date(),
+        taskId: 6,
+      },
+    },
+  ];
+
+  const mockTrackerService = {
+    getCurrentTask: jest
+      .fn()
+      .mockResolvedValue(task)
+      .mockResolvedValueOnce(null),
+
+    getFinishedTasks: jest
+      .fn()
+      .mockResolvedValue(finishedTasks)
+      .mockResolvedValueOnce(null),
+
+    timer: jest.fn().mockImplementation((taskId) => {
+      return Promise.resolve({ ...timerInfo, taskId: taskId });
+    }),
+
+    stopCurrentTask: jest
+      .fn()
+      .mockResolvedValue(finishedTasks[1])
+      .mockResolvedValueOnce(null),
+
+    addNewTask: jest.fn().mockImplementation((data) => {
+      return {
+        id: 10,
+        ...data,
+        status: Status.ACTIVE,
+        timer: {
+          ...timerInfo,
+          statusId: 10,
+        },
+      };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrackerResolver],
-    }).compile();
+      providers: [TrackerResolver, TrackerService],
+    })
+      .overrideProvider(TrackerService)
+      .useValue(mockTrackerService)
+      .compile();
 
     resolver = module.get<TrackerResolver>(TrackerResolver);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should return currently running task - null case', async () => {
+    expect(await resolver.fetchCurrentTask()).toEqual(null);
+
+    expect(mockTrackerService.getCurrentTask).toHaveBeenCalled();
+  });
+
+  it('should return currently running task', async () => {
+    expect(await resolver.fetchCurrentTask()).toEqual({
+      ...task,
+    });
+
+    expect(mockTrackerService.getCurrentTask).toHaveBeenCalled();
+  });
+
+  it('should return already finished tasks - null case', async () => {
+    expect(await resolver.getFinishedTasks()).toEqual(null);
+
+    expect(mockTrackerService.getFinishedTasks).toHaveBeenCalled();
+  });
+
+  it('should return already finished tasks', async () => {
+    expect(await resolver.getFinishedTasks()).toEqual(finishedTasks);
+
+    expect(mockTrackerService.getFinishedTasks).toHaveBeenCalled();
+  });
+
+  it('should return timer resolved field', async () => {
+    expect(await resolver.timer(task)).toEqual(task.timer);
+
+    expect(mockTrackerService.timer).toHaveBeenCalled();
+  });
+
+  it('should return stopped Task - null case', async () => {
+    expect(await resolver.stopCurrentTask()).toEqual(null);
+
+    expect(mockTrackerService.stopCurrentTask).toHaveBeenCalled();
+  });
+
+  it('should return stopped Task', async () => {
+    expect(await resolver.stopCurrentTask()).toEqual(finishedTasks[1]);
+
+    expect(mockTrackerService.stopCurrentTask).toHaveBeenCalled();
+  });
+
+  it('should return new Task', async () => {
+    const data = {
+      name: 'New Task',
+      description: 'With description',
+    };
+
+    expect(await resolver.addNewTask(data)).toEqual({
+      id: 10,
+      ...data,
+      status: Status.ACTIVE,
+      timer: {
+        ...timerInfo,
+        statusId: 10,
+      },
+    });
+
+    expect(mockTrackerService.addNewTask).toHaveBeenCalled();
   });
 });
